65ca67efc80ea24c39556442b780f87a499ca587
yasmin afifi <67705548+yasmin-afifi@users.noreply.github.com>
2024-12-30 20:19:11 +0200

    Add files via upload

---
+++ Python Tutorial/Calculate Factorial Using Recursion/main.py
@@ -0,0 +1,24 @@
+def main():
+    number = 0
+    while True:
+        try:
+            number = int(input('Enter a positive number: '))
+            if number < 0:
+                raise ValueError("please enter a positive number")
+        except:
+            print('please enter a positive number')
+        else:
+            break
+
+    def factorial(number):
+        if number == 0 or number == 1:
+            return 1
+        else:
+            return number*factorial(number-1)
+
+    factorial_result = factorial(number)
+    return number, factorial_result
+
+
+number, fact = main()
+print(f"factorial of {number} = {fact}")
